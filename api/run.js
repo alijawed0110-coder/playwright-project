dedee35bf3a04a796bb612c0e221719bb754042b
Ali Jawed <alijawed0110@gmail.com>
2026-05-11 15:59:04 +0500

    Update run.js

--- api/run.js
+++ api/run.js
@@ -1,11 +1,28 @@
+// import { exec } from "child_process";
+
+// export default function handler(req, res) {
+//   exec("npx playwright test tests/speaker.spec.js", (err, stdout, stderr) => {
+//     if (err) {
+//       return res.status(500).json({
+//         success: false,
+//         error: stderr
+//       });
+//     }
+
+//     res.status(200).json({
+//       success: true,
+//       output: stdout
+//     });
+//   });
+// }
 import { exec } from "child_process";
 
 export default function handler(req, res) {
-  exec("npx playwright test tests/speaker.spec.js --headed", (err, stdout, stderr) => {
+  exec("npx playwright test tests/speaker.spec.js --reporter=list", (err, stdout, stderr) => {
     if (err) {
       return res.status(500).json({
         success: false,
-        error: stderr
+        error: stderr || err.message
       });
     }
 
